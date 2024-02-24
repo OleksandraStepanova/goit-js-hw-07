d5ad3606ce6340931b4061c474a79568aa94d41f
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-02-24 22:04:14 +0100

    add task2

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const categories = document.querySelector("#categories");
+
+
+const number = categories.children.length;
+console.log("Number of categories: " + number);
+
+const elements = document.querySelectorAll(".item");
+
+elements.forEach(function (element) {
+    const textTitle = element.firstElementChild.textContent;
+    console.log("Category: " + textTitle);
+    const numberChild = element.lastElementChild.children.length;
+    console.log("Elements: " + numberChild);
+})
+
