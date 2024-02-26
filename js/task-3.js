6216c3d31d9ed6c4adb2a864910ffec6776dcc73
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-02-26 19:53:30 +0100

    add trim()

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,18 @@
-const newText = document.querySelector("#name-input");
-const hello = document.querySelector("#name-output");
-newText.addEventListener("input", getName);
-function getName(event) {    
-    
-    const input = event.currentTarget.value;
-    const arr = input.split('');
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-    if (!arr.every(v => v === " ")) {
-         hello.innerHTML = event.currentTarget.value.trim();
+nameInput.addEventListener("input", updateNameOutput);
+
+function updateNameOutput(event) {    
+    
+    const input = event.currentTarget.value.trim();
+    console.log(input)
+    
+    if (input===``) {        
+        nameOutput.innerHTML = 'Anonymous';
     }
     else {
-        hello.innerHTML = 'Anonymous';
+        nameOutput.innerHTML = `${input}`;
     }
    
 }
