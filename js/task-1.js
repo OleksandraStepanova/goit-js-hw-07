a4081988cdedd73269ee4514b4cb5f085e71a6bf
Oleksandra Stepanova <oleksandra.stepanova@gmail.com>
2025-05-13 16:40:55 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,27 @@
 const categories = document.querySelector("#categories");
+// if (categories) {
+    
+//     const categoriesItem = document.querySelectorAll("#categories>li.item");
+    
+//     const numberOfCatecories = categoriesItem.length;
+//     console.log(numberOfCatecories);
+//     categoriesItem.forEach(categoriesItem => {
+//         const categoryTytleElement = categoriesItem.querySelector("h2")
+//         const categoryTytle = categoryTytleElement ? categoryTytleElement.textContent : "error"
+//         const categoryListElement = categoriesItem.querySelector("ul")
+//         const categoryList = categoryListElement ? categoryListElement.querySelectorAll('li') : []
+//         const number = categoryList.length;
+//         console.log(categoryTytle);
+//         console.log(number);
+//     });
+    
+    
+    
+// } else {
+//     console.log(false);
+    
+// }
+
 
 
 const number = categories.children.length;
